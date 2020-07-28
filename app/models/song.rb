c08b0627845b44c065234931af574dfa69792eef
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-28 18:05:34 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,5 @@ class Song < ActiveRecord::Base
     belongs_to :genre 
     
     def artist_name=(name)
-      self.artist = Artist.create_or_find_by
+      self.artist = Artist.create_or_find_by(name: name)
 end
